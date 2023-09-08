26d1b6a2081742a2798071c0da9b1144a8c8c71c
FakeEx1stence <vitaliishaenko@gmail.com>
2023-09-09 01:21:15 +0300

    Second homework

---
+++ lab/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float a = 0; // initiating 
+    cout << "A = ";
+    cin >> a;
+
+    float res = a - 5; 
+    cout << a << " - 5 is " << res << endl;
+    system("pause");
+    return 0;
+}
