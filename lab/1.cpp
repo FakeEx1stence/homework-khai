1f02a8df270059e58fe693c8a0d28fb1c9dfaf14
FakeEx1stence <vitaliishaenko@gmail.com>
2023-09-09 01:06:40 +0300

    Hello World

---
+++ lab/1.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //i/o library
+
+using namespace std; //instead od prefix std::
+
+int main()//header
+{  //begin
+   cout << "Hello, World" << endl;
+    system("Pause");
+    return 0; //return to OS
+}  // end
